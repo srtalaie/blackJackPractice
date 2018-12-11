2bb87f2cd14f2460c3ac819226810e17a8abc987
srtalaie <srt4721@yahoo.com>
2018-12-11 11:41:17 -0800

    set up file structure

---
+++ server.js
@@ -0,0 +1,26 @@
+//Dependencies
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+//Middleware for data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//Routes for Pages
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get('/styles', function(req, res){
+    res.sendFile(path.join(__dirname, "./public/assets/css/style.css"));
+});
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
