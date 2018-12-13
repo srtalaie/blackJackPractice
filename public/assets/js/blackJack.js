df34813d841fa5fb2bcde4dcf23bf0b9e2007a6d
srtalaie <srt4721@yahoo.com>
2018-12-13 13:24:05 -0800

    can calculate dealer total properly

--- public/assets/js/blackJack.js
+++ public/assets/js/blackJack.js
@@ -8,12 +8,16 @@ function Cards(value, suit){
 
 let newCard = new Cards(possibleValues[(Math.floor(Math.random() * possibleValues.length))], suits[(Math.floor(Math.random() * suits.length))]);
 
-console.log(newCard);
-
 function Dealer(card1, card2){
+    this.total = 0;
     this.dealerCards = [card1, card2],
     this.shownCard = this.dealerCards[0],
-    this.totalValue = this.dealerCards.reduce((a, b) => { return a.value + b.value }),
+    this.totalValue = function(){
+      for(let i = 0; i < this.dealerCards.length; i++){
+        this.total += this.dealerCards[i].value;
+      }
+      return this.total;
+    }
     this.dealerHit = function(){
       let newCard = new Cards(possibleValues[(Math.floor(Math.random() * possibleValues.length))], suits[(Math.floor(Math.random() * suits.length))]);
       this.dealerCards.push(newCard)
@@ -24,11 +28,10 @@ let initialDealerCard1 = new Cards(possibleValues[(Math.floor(Math.random() * po
 let initialDealerCard2 = new Cards(possibleValues[(Math.floor(Math.random() * possibleValues.length))], suits[(Math.floor(Math.random() * suits.length))]);
 
 let dealer1 = new Dealer(initialDealerCard1, initialDealerCard2);
-console.log(dealer1.totalValue, dealer1.dealerCards);
 
 dealer1.dealerHit();
-
-console.log(dealer1.totalValue, dealer1.dealerCards, dealer1.dealerCards.reduce((a, b) => a.value + b.value ));
+dealer1.totalValue();
+console.log(dealer1.total, dealer1.dealerCards);
 
 // if(this.totalValue <= 17 && this.dealerCards[0] === 11 || this.dealerCards[1] === 11){
 //     this.totalValue = this.totalValue - 10;
