eb2769fb00ee8c4ad18c8e7ede733e53093c6f98
srtalaie <srt4721@yahoo.com>
2018-12-13 14:01:42 -0800

    added comments

--- public/assets/js/blackJack.js
+++ public/assets/js/blackJack.js
@@ -1,16 +1,18 @@
+//Create "deck" variables we will be using
 let possibleValues = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 let suits = ["hearts", "diamonds", "spades", "clubs"];
 
+//Create the card object to be used for both player's cards and dealer's cards
 function Cards(value, suit){
     this.value = value,
     this.suit = suit
 }
 
-let newCard = new Cards(possibleValues[(Math.floor(Math.random() * possibleValues.length))], suits[(Math.floor(Math.random() * suits.length))]);
-
+//Create Dealer object and its various actions
 function Dealer(card1, card2){
     this.total = 0;
     this.dealerCards = [card1, card2],
+    //Card dealer will be showing
     this.shownCard = this.dealerCards[0],
     this.totalValue = function(){
       for(let i = 0; i < this.dealerCards.length; i++){
@@ -24,16 +26,13 @@ function Dealer(card1, card2){
     }
 }
 
+//Intialize a new dealer
 let initialDealerCard1 = new Cards(possibleValues[(Math.floor(Math.random() * possibleValues.length))], suits[(Math.floor(Math.random() * suits.length))]);
 let initialDealerCard2 = new Cards(possibleValues[(Math.floor(Math.random() * possibleValues.length))], suits[(Math.floor(Math.random() * suits.length))]);
 
 let dealer1 = new Dealer(initialDealerCard1, initialDealerCard2);
 
-dealer1.dealerHit();
-dealer1.totalValue();
-console.log(dealer1.total, dealer1.dealerCards);
-
-
+//Function to check dealer's total and execute various dealer actions based on that total
 function checkDealerTotal(dealer){
     if(dealer.total === 21){
       return dealer.total;
@@ -55,18 +54,3 @@ function checkDealerTotal(dealer){
       return dealer.total;
     }
 }
-
-// if(this.totalValue <= 17 && this.dealerCards[0] === 11 || this.dealerCards[1] === 11){
-//     this.totalValue = this.totalValue - 10;
-//     let newCard = new Cards(possibleValues[(Math.floor(Math.random() * possibleValues.length))], suits[(Math.floor(Math.random() * suits.length))]);
-//     this.dealerCards.push(newCard);
-//     return this.totalValue;
-// } else if(this.totalValue < 17){
-//     let newCard = new Cards(possibleValues[(Math.floor(Math.random() * possibleValues.length))], suits[(Math.floor(Math.random() * suits.length))]);
-//     this.dealerCards.push(newCard);
-//     return this.totalValue;
-// } else if(this.totalValue >= 17 && this.totalValue < 22){
-//     return this.totalValue;
-// } else if(this.totalValue > 21){
-//     return this.totalValue;
-// }
