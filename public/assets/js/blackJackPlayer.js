02eb58c27c926bdf641d9bab1adde72dfc9d6d1d
srtalaie <srt4721@yahoo.com>
2018-12-26 11:52:46 -0800

    create player object

---
+++ public/assets/js/blackJackPlayer.js
@@ -0,0 +1,32 @@
+let Cards = require("./card.js");
+
+// function Cards(value, suit){
+//   this.value = value,
+//   this.suit = suit
+// }
+
+//Create "deck" variables we will be using
+let possibleValues = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+let suits = ["hearts", "diamonds", "spades", "clubs"];
+
+//Create Dealer object and its various actions
+function Player(card1, card2){
+    this.total = 0,
+    this.playerCards = [card1, card2],
+    //Card player will be showing
+    this.shownCard = this.playerCards[0],
+    this.totalValue = function(){
+      this.total = 0;
+      for(let i = 0; i < this.playerCards.length; i++){
+        this.total += this.playerCards[i].value;
+      }
+      return this.total;
+    }
+    this.hitCounter = 0,
+    this.playerHit = function(){
+      this.hitCounter = 0;
+      let newCard = new Cards(possibleValues[(Math.floor(Math.random() * possibleValues.length))], suits[(Math.floor(Math.random() * suits.length))]);
+      this.playerCards.push(newCard)
+      this.hitCounter++;
+    }
+  }
