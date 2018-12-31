35930649172f607cc34dbe7090c288512819351b
srtalaie <srt4721@yahoo.com>
2018-12-30 18:11:44 -0800

    Clean up player js

--- public/assets/js/blackJackPlayer.js
+++ public/assets/js/blackJackPlayer.js
@@ -1,10 +1,5 @@
 let Cards = require("./card.js");
 
-// function Cards(value, suit){
-//   this.value = value,
-//   this.suit = suit
-// }
-
 //Create "deck" variables we will be using
 let possibleValues = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 let suits = ["hearts", "diamonds", "spades", "clubs"];
@@ -49,9 +44,3 @@ function Player(card1, card2, bet){
         }
   }
 }
-
-//Intialize a new dealer
-let initialPlayerCard1 = new Cards(possibleValues[(Math.floor(Math.random() * possibleValues.length))], suits[(Math.floor(Math.random() * suits.length))]);
-let initialPlayerCard2 = new Cards(possibleValues[(Math.floor(Math.random() * possibleValues.length))], suits[(Math.floor(Math.random() * suits.length))]);
-
-let player1 = new Player(initialPlayerCard1, initialPlayerCard2, 10);
